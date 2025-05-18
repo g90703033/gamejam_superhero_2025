26c2a7d91772a1f0c87a152eead4ed16d5273a6f
LinJason <linjason0209@gmail.com>
2025-05-18 13:20:23 +0800

    Add Hand broken

--- Assets/Avatar/Script/LimbHeader.cs
+++ Assets/Avatar/Script/LimbHeader.cs
@@ -8,11 +8,21 @@ namespace Jason.Avatar
         [HideInInspector] public string BuffID;
         [HideInInspector] public LimbTag limbTag = new();
         [HideInInspector] public PlayerAttribute Attribute;
+        public HeroArm HeroArm;
         public class LimbTag
         {
             public LimbType limbType;
             public ModifyType modifyType;
             public LevelType levelType;
+        }
+        private void Update()
+        {
+            if(HeroArm is not null)
+            ChangeWeightColor(HeroArm.holdingWeight);
+        }
+        public void ChangeWeightColor(float value)
+        {
+            //Debug.LogWarning($"[Arm] {HeroArm.armType} {value}");
         }
     }
 }
