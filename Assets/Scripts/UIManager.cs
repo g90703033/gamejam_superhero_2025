348bf225227287774b406a796ab6bd6971a9cd29
g90703033 <g90703033@gmail.com>
2025-05-18 16:46:19 +0800

    merge ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -8,16 +7,13 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-
     //各種 Panel 以及 UI
     public GameObject mainMenuPanel;
     public GameObject pausePanel;
     public GameObject hud1PPanel;
     public GameObject hud2PPanel;
     public GameObject abilityChoosePanel;
-    public Slider powerSlide;
     public TextMeshProUGUI timerText;
-    public List<BuffAttributeList> ability = new List<BuffAttributeList>();
     public BuffManager buffManager;
     public List<Image> bufferSelectionImage;
     public List<TextMeshProUGUI> bufferSelectionTitle;
@@ -28,7 +24,7 @@ public class UIManager : MonoBehaviour
 
     //確認是否為單人遊戲
     private bool isSinglePlayerGame = true;
-    
+
     //確認 1P/2P 以及所有玩家是否已經選完能力
     public bool is1PReadyToGetAbility = true;
     public bool is2PReadyToGetAbility = true;
@@ -44,10 +40,11 @@ public class UIManager : MonoBehaviour
     //累積值
     private int powerValue = 0;
     public int powerThreshold = 100;
-    
+
     //計時
     public float timerThrshold = 20f;
     private float timer = 0f;
+
     public Slider powerSlide;
 
     void Start()
@@ -89,8 +86,6 @@ public class UIManager : MonoBehaviour
         hud1PPanel.SetActive(false);
         hud2PPanel.SetActive(true);
         powerSlide = powervalueSliders[1];
-        powerSlide.value = powerValue;
-        powerSlide.maxValue = powerThreshold;
         isSinglePlayerGame = false;
         isTimerStart = true;
         timer = timerThrshold;
@@ -107,10 +102,12 @@ public class UIManager : MonoBehaviour
     public void ToggleEnd()
     {
         isTimerStart = false;
-        if(isSinglePlayerGame == true){
+        if (isSinglePlayerGame == true)
+        {
             hud1PPanel.SetActive(false);
         }
-        else{
+        else
+        {
             hud2PPanel.SetActive(false);
         }
         pausePanel.SetActive(true);
@@ -121,10 +118,12 @@ public class UIManager : MonoBehaviour
     public void ResumeGame()
     {
         pausePanel.SetActive(false);
-        if(isSinglePlayerGame == true){
+        if (isSinglePlayerGame == true)
+        {
             hud1PPanel.SetActive(true);
         }
-        else{
+        else
+        {
             hud2PPanel.SetActive(true);
         }
         Time.timeScale = 1f;
@@ -138,17 +137,19 @@ public class UIManager : MonoBehaviour
     {
         List<BuffAttribute> bufferSelection = new List<BuffAttribute>();
         bufferSelection = buffManager.GetThreeRandomAttribute();
-        for(int i = 0; i < bufferSelectionImage.Count; i++){
+        for (int i = 0; i < bufferSelectionImage.Count; i++)
+        {
             bufferSelectionImage[i].sprite = bufferSelection[i].icon;
             bufferSelectionTitle[i].text = bufferSelection[i].title;
             bufferSelectionDescription[i].text = $"Lift:{bufferSelection[i].attributes.weightLifting} <br> Speed:{bufferSelection[i].attributes.moveSpeed}";
         }
 
-        if(isSinglePlayerGame == true){
+        if (isSinglePlayerGame == true)
+        {
             hud1PPanel.SetActive(false);
-            GameState.Instance.DestroyP2();
         }
-        else{
+        else
+        {
             hud2PPanel.SetActive(false);
         }
         selection1P.anchoredPosition = new Vector2(bufferSelectionImage[currentAbility1PSelect].GetComponent<RectTransform>().anchoredPosition.x, selection1P.anchoredPosition.y);
@@ -163,10 +164,12 @@ public class UIManager : MonoBehaviour
     //關閉能力選單畫面
     public void StopChoosingAbility()
     {
-        if(isSinglePlayerGame == true){
+        if (isSinglePlayerGame == true)
+        {
             hud1PPanel.SetActive(true);
         }
-        else{
+        else
+        {
             hud2PPanel.SetActive(true);
         }
         abilityChoosePanel.SetActive(false);
@@ -174,9 +177,11 @@ public class UIManager : MonoBehaviour
     }
 
     //累積值操作
-    public void PowerValueOperation(int value){
+    public void PowerValueOperation(int value)
+    {
         powerValue = powerValue + value;
-        if (powerValue >= powerThreshold){
+        if (powerValue >= powerThreshold)
+        {
             StartChoosingAbility();
             powerValue = 0;
 
@@ -185,98 +190,123 @@ public class UIManager : MonoBehaviour
     }
 
     //計時器顯示
-    void DisplayTimerText(){
-        int minutes = Mathf.FloorToInt(timer/60);
-        int seconds = Mathf.FloorToInt(timer%60);
+    void DisplayTimerText()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
         timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         //計時
-        if(timer > 0 && isTimerStart == true)
+        if (timer > 0 && isTimerStart == true)
         {
             timer = timer - Time.fixedDeltaTime * Time.timeScale;
             DisplayTimerText();
         }
-        else if (isTimerStart == true){
+        else if (isTimerStart == true)
+        {
             ToggleEnd();
         }
     }
-    void Update(){
+    void Update()
+    {
 
         //能力選單選擇邏輯
-        if(isAllReadyToGetAbility == false){
-            if(isSinglePlayerGame == true){
+        if (isAllReadyToGetAbility == false)
+        {
+            if (isSinglePlayerGame == true)
+            {
                 is2PReadyToGetAbility = true;
                 currentAbility2PSelect = 3;
                 selection2P.gameObject.SetActive(false);
             }
 
-            if(Keyboard.current[Key.A].wasPressedThisFrame && is1PReadyToGetAbility == false){
+            if (Keyboard.current[Key.A].wasPressedThisFrame && is1PReadyToGetAbility == false)
+            {
                 currentAbility1PSelect = currentAbility1PSelect - 1;
-                if(currentAbility1PSelect < 0){
+                if (currentAbility1PSelect < 0)
+                {
                     currentAbility1PSelect = 2;
                 }
-                if(currentAbility1PSelect == currentAbility2PSelect){
+                if (currentAbility1PSelect == currentAbility2PSelect)
+                {
                     currentAbility1PSelect = currentAbility1PSelect - 1;
                 }
-                if(currentAbility1PSelect < 0){
+                if (currentAbility1PSelect < 0)
+                {
                     currentAbility1PSelect = 2;
                 }
                 selection1P.anchoredPosition = new Vector2(bufferSelectionImage[currentAbility1PSelect].GetComponent<RectTransform>().anchoredPosition.x, selection1P.anchoredPosition.y);
             }
 
-            if(Keyboard.current[Key.D].wasPressedThisFrame && is1PReadyToGetAbility == false){
+            if (Keyboard.current[Key.D].wasPressedThisFrame && is1PReadyToGetAbility == false)
+            {
                 currentAbility1PSelect = currentAbility1PSelect + 1;
-                if(currentAbility1PSelect > 2){
+                if (currentAbility1PSelect > 2)
+                {
                     currentAbility1PSelect = 0;
                 }
-                if( currentAbility1PSelect == currentAbility2PSelect ){
+                if (currentAbility1PSelect == currentAbility2PSelect)
+                {
                     currentAbility1PSelect = currentAbility1PSelect + 1;
                 }
-                if(currentAbility1PSelect > 2){
+                if (currentAbility1PSelect > 2)
+                {
                     currentAbility1PSelect = 0;
                 }
                 selection1P.anchoredPosition = new Vector2(bufferSelectionImage[currentAbility1PSelect].GetComponent<RectTransform>().anchoredPosition.x, selection1P.anchoredPosition.y);
             }
 
-            if(Keyboard.current[Key.R].wasPressedThisFrame){
+            if (Keyboard.current[Key.R].wasPressedThisFrame)
+            {
                 is1PReadyToGetAbility = !is1PReadyToGetAbility;
             }
 
-            if(Keyboard.current[Key.J].wasPressedThisFrame && is2PReadyToGetAbility == false){
+            if (Keyboard.current[Key.J].wasPressedThisFrame && is2PReadyToGetAbility == false)
+            {
                 currentAbility2PSelect = currentAbility2PSelect - 1;
-                if(currentAbility2PSelect < 0){
+                if (currentAbility2PSelect < 0)
+                {
                     currentAbility2PSelect = 2;
                 }
-                if(currentAbility2PSelect == currentAbility1PSelect){
+                if (currentAbility2PSelect == currentAbility1PSelect)
+                {
                     currentAbility2PSelect = currentAbility2PSelect - 1;
                 }
-                if(currentAbility2PSelect < 0){
+                if (currentAbility2PSelect < 0)
+                {
                     currentAbility2PSelect = 2;
                 }
                 selection2P.anchoredPosition = new Vector2(bufferSelectionImage[currentAbility2PSelect].GetComponent<RectTransform>().anchoredPosition.x, selection2P.anchoredPosition.y);
             }
 
-            if(Keyboard.current[Key.L].wasPressedThisFrame && is2PReadyToGetAbility == false){
+            if (Keyboard.current[Key.L].wasPressedThisFrame && is2PReadyToGetAbility == false)
+            {
                 currentAbility2PSelect = currentAbility2PSelect + 1;
-                if(currentAbility2PSelect > 2){
+                if (currentAbility2PSelect > 2)
+                {
                     currentAbility2PSelect = 0;
                 }
-                if( currentAbility2PSelect == currentAbility1PSelect ){
+                if (currentAbility2PSelect == currentAbility1PSelect)
+                {
                     currentAbility2PSelect = currentAbility2PSelect + 1;
                 }
-                if(currentAbility2PSelect > 2){
+                if (currentAbility2PSelect > 2)
+                {
                     currentAbility2PSelect = 0;
                 }
                 selection2P.anchoredPosition = new Vector2(bufferSelectionImage[currentAbility2PSelect].GetComponent<RectTransform>().anchoredPosition.x, selection2P.anchoredPosition.y);
             }
 
-            if(Keyboard.current[Key.P].wasPressedThisFrame){
+            if (Keyboard.current[Key.P].wasPressedThisFrame)
+            {
                 is2PReadyToGetAbility = !is2PReadyToGetAbility;
             }
 
-            if(is1PReadyToGetAbility == true && is2PReadyToGetAbility == true){
+            if (is1PReadyToGetAbility == true && is2PReadyToGetAbility == true)
+            {
                 isAllReadyToGetAbility = true;
                 StopChoosingAbility();
 
@@ -289,7 +319,7 @@ public class UIManager : MonoBehaviour
                 if (is2PReadyToGetAbility)
                 {
                     if (currentAbility2PSelect < 3 && currentAbility2PSelect >= 0)
-                    GameState.Instance.OnP2BuffGet(bufferSelectionTitle[currentAbility2PSelect].text.Trim());
+                        GameState.Instance.OnP2BuffGet(bufferSelectionTitle[currentAbility2PSelect].text.Trim());
                 }
             }
 
